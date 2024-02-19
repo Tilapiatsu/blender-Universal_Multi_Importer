eaeff27caf02734012d4deb925df26a2a42bcdba
Tilapiatsu <tilapiatsu@hotmail.fr>
2024-02-19 22:31:01 +0100

    * append from other blend file is working

--- formats/import_module/blend_format.py
+++ formats/import_module/blend_format.py
@@ -1,5 +1,5 @@
 import bpy
-import os
+from .unique_name import UniqueName
 
 
 class IMPORT_SCENE_OT_tila_import_blend(bpy.types.Operator):
@@ -9,137 +9,191 @@ class IMPORT_SCENE_OT_tila_import_blend(bpy.types.Operator):
 	bl_description = 'Append or Link Blend file'
 
 	import_mode : bpy.props.EnumProperty(name="Import Mode", default="APPEND", items=[("APPEND", "Append", ""), ("LINK", "Link", "")])
-	import_action : bpy.props.BoolProperty(name="Import Actions", default=False)
-	import_armature : bpy.props.BoolProperty(name="Import Armatures", default=False)
-	import_brush : bpy.props.BoolProperty(name="Import Brushes", default=False)
-	import_camera : bpy.props.BoolProperty(name="Import Cameras", default=False)
-	import_cache_file : bpy.props.BoolProperty(name="Import Cache files",  default=False)
-	import_curve : bpy.props.BoolProperty(name="Import Curves", default=False)
-	import_font : bpy.props.BoolProperty(name="Import Fonts", default=False)
-	import_grease_pencil : bpy.props.BoolProperty(name="Import Grease Penil", default=False)
-	import_collection : bpy.props.BoolProperty(name="Import Collections", default=False)
-	import_image : bpy.props.BoolProperty(name="Import Images", default=False)
-	import_light : bpy.props.BoolProperty(name="Import Lights", default=False)
-	import_line_style : bpy.props.BoolProperty(name="Import Line Styes", default=False)
-	import_lattice : bpy.props.BoolProperty(name="Import Lattices", default=False)
-	import_mask : bpy.props.BoolProperty(name="Import Masks", default=False)
-	import_material : bpy.props.BoolProperty(name="Import Materials", default=False)
-	import_metaball : bpy.props.BoolProperty(name="Import Metaballs", default=False)
-	import_mesh : bpy.props.BoolProperty(name="Import Meshes", default=False)
-	import_movie_clip : bpy.props.BoolProperty(name="Import Movie Clips", default=False)
-	import_node_tree : bpy.props.BoolProperty(name="Import Node Trees", default=False)
-	import_object : bpy.props.BoolProperty(name="Import Objects", default=True)
-	import_paint_curve : bpy.props.BoolProperty(name="Import Paint Cures", default=False)
-	import_palette : bpy.props.BoolProperty(name="Import Palettes", default=False)
-	import_particle : bpy.props.BoolProperty(name="Import Particle", default=False)
-	import_point_cloud : bpy.props.BoolProperty(name="Import Point Cloud", default=False)
-	import_light_probe : bpy.props.BoolProperty(name="Import Light Probes", default=False)
-	import_scene : bpy.props.BoolProperty(name="Import Scenes", default=False)
-	import_sound : bpy.props.BoolProperty(name="Import Sounds", default=False)
-	import_speaker : bpy.props.BoolProperty(name="Import Speakers", default=False)
-	import_text : bpy.props.BoolProperty(name="Import Texts", default=False)
-	import_texture : bpy.props.BoolProperty(name="Import Textures", default=False)
+
+	import_actions : bpy.props.BoolProperty(name="Import Actions", default=False)
+	import_armatures : bpy.props.BoolProperty(name="Import Armatures", default=False)
+	import_brushes : bpy.props.BoolProperty(name="Import Brushes", default=False)
+	import_cameras : bpy.props.BoolProperty(name="Import Cameras", default=False)
+	import_cache_files : bpy.props.BoolProperty(name="Import Cache files",  default=False)
+	import_curves : bpy.props.BoolProperty(name="Import Curves", default=False)
+	import_hair_curves : bpy.props.BoolProperty(name="Import Curves", default=False)
+	import_fonts : bpy.props.BoolProperty(name="Import Fonts", default=False)
+	import_grease_pencils : bpy.props.BoolProperty(name="Import Grease Penil", default=False)
+	import_collections : bpy.props.BoolProperty(name="Import Collections", default=False)
+	import_images : bpy.props.BoolProperty(name="Import Images", default=False)
+	import_lights : bpy.props.BoolProperty(name="Import Lights", default=False)
+	import_linestyles : bpy.props.BoolProperty(name="Import Line Styes", default=False)
+	import_lattices : bpy.props.BoolProperty(name="Import Lattices", default=False)
+	import_masks : bpy.props.BoolProperty(name="Import Masks", default=False)
+	import_materials : bpy.props.BoolProperty(name="Import Materials", default=False)
+	import_metaballs : bpy.props.BoolProperty(name="Import Metaballs", default=False)
+	import_meshes : bpy.props.BoolProperty(name="Import Meshes", default=False)
+	import_movieclips : bpy.props.BoolProperty(name="Import Movie Clips", default=False)
+	import_node_groups : bpy.props.BoolProperty(name="Import Node Groups", default=False)
+	import_objects : bpy.props.BoolProperty(name="Import Objects", default=True)
+	import_paint_curves : bpy.props.BoolProperty(name="Import Paint Cures", default=False)
+	import_palettes : bpy.props.BoolProperty(name="Import Palettes", default=False)
+	import_particles : bpy.props.BoolProperty(name="Import Particle", default=False)
+	import_pointclouds : bpy.props.BoolProperty(name="Import Point Cloud", default=False)
+	import_lightprobes : bpy.props.BoolProperty(name="Import Light Probes", default=False)
+	import_scenes : bpy.props.BoolProperty(name="Import Scenes", default=False)
+	import_sounds : bpy.props.BoolProperty(name="Import Sounds", default=False)
+	import_speakers : bpy.props.BoolProperty(name="Import Speakers", default=False)
+	import_texts : bpy.props.BoolProperty(name="Import Texts", default=False)
+	import_textures : bpy.props.BoolProperty(name="Import Textures", default=False)
 	import_volumes : bpy.props.BoolProperty(name="Import Volumes", default=False)
-	import_world : bpy.props.BoolProperty(name="Import Worlds", default=False)
-	import_workspace : bpy.props.BoolProperty(name="Import Workspaces", default=False)
+	import_worlds : bpy.props.BoolProperty(name="Import Worlds", default=False)
+	import_workspaces : bpy.props.BoolProperty(name="Import Workspaces", default=False)
 	
 	filepath : bpy.props.StringProperty(name="File Path", subtype='FILE_PATH', options={'HIDDEN'})
 
 	import_to_collection_source = ['objects']
+	excluded_datatypes = ['batch_remove', 'bl_rna', 'filepath', 'is_dirty', 'is_saved', 'libraries', 'orphans_purge', 'rna_type', 'screens', 'temp_data', 'use_autopack', 'user_map', 'version', 'window_managers']
+	
+	@property
+	def import_datas(self):
+		if self._import_datas is None:
+			self._import_datas = [m.replace('import_', '') for m in self.__annotations__.keys() if m.startswith('import_') and m != 'import_mode' and getattr(self, m)]
+		
+		return self._import_datas
+
+	@property
+	def local_names(self):
+		if self._local_names is None:
+			local_names = {}
+
+			for datatype in dir(bpy.data):
+				if (datatype in self.excluded_datatypes or datatype.startswith('__')) or datatype not in self.import_datas :
+					continue
+				
+				data = getattr(bpy.data, datatype)
+				
+				local_names[datatype] = [d.name for d in data]
+			
+			self._local_names = local_names
+			
+		return self._local_names
+
+	def register_local_unique_names(self):
+		for source in self.import_datas:
+			local_datas = getattr(bpy.data, source)
+
+			for e in local_datas:
+				self.unique_name.register_element_correspondance(e)
+
 
 	def get_import_command(self):
 		return bpy.ops.wm.append if self.import_mode == "APPEND" else bpy.ops.wm.link
 	
 	def import_command(self, source):
 		imported_objects = []
+		local_datas = getattr(bpy.data, source)
 		with bpy.data.libraries.load(self.filepath, link=self.import_mode == 'LINK') as (data_from, data_to):
 			for name in getattr(data_from, source):
-				if name not in data_to:
+				target = getattr(data_to, source)
+				if name not in self.local_names[source]:
 					# Import objects
 					print(f'Importing {source} : {name}')
-					getattr(data_to, source).append(name)
+					target.append(name)
 				else:
-					# TODO : Need to deal with Name Collision
-					pass
+					self.unique_name.register_element_correspondance(local_datas[name])
+					new_name = self.unique_name.get_unique_name(name)
+					local_datas[name].name = new_name
+					target.append(name)
 
-				if source in self.import_to_collection_source:
-					imported_objects.append(name)
-		
+		for element, name in self.unique_name.element_correspondance.items():
+			new_name = self.unique_name.get_next_valid_name(name)
+			local_datas[name].name = new_name
+			element.name = name
+
+			if source in self.import_to_collection_source:
+				imported_objects.append(new_name)
+
+		print(imported_objects)
 		for name in imported_objects:
-			self.current_collection.objects.link(bpy.data.objects[name])
+			if name in self.current_collection.objects.keys():
+				continue
+			self.current_collection.objects.link(local_datas[name])
 
 	def execute(self, context):
 		self.current_collection = context.collection
-		if self.import_action:
+		self.unique_name = UniqueName()
+		self._import_datas = None
+		self._local_names = None
+		self.register_local_unique_names()
+
+		if self.import_actions:
 			self.import_command('actions')
-		if self.import_armature:
+		if self.import_armatures:
 			self.import_command('armatures')
-		if self.import_brush:
+		if self.import_brushes:
 			self.import_command('brushes')
-		if self.import_camera:
+		if self.import_cameras:
 			self.import_command('cameras')
-		if self.import_cache_file:
+		if self.import_cache_files:
 			self.import_command('cache_files')
-		if self.import_curve:
+		if self.import_curves:
 			self.import_command('curves')
+		if self.import_hair_curves:
 			self.import_command('hair_curves')
-		if self.import_font:
+		if self.import_fonts:
 			self.import_command('fonts')
-		if self.import_grease_pencil:
+		if self.import_grease_pencils:
 			self.import_command('grease_pencils')
 			self.import_command('grease_pencils_v3')
-		if self.import_collection:
+		if self.import_collections:
 			self.import_command('collections')
-		if self.import_image:
+		if self.import_images:
 			self.import_command('images')
-		if self.import_light:
+		if self.import_lights:
 			self.import_command('lights')
-		if self.import_line_style:
+		if self.import_linestyles:
 			self.import_command('linestyles')
-		if self.import_lattice:
+		if self.import_lattices:
 			self.import_command('lattices')
-		if self.import_mask:
+		if self.import_masks:
 			self.import_command('masks')
-		if self.import_material:
+		if self.import_materials:
 			self.import_command('materials')
-		if self.import_metaball:
+		if self.import_metaballs:
 			self.import_command('metaballs')
-		if self.import_mesh:
+		if self.import_meshes:
 			self.import_command('meshes')
-		if self.import_movie_clip:
+		if self.import_movieclips:
 			self.import_command('movieclips')
-		if self.import_node_tree:
+		if self.import_node_groups:
 			self.import_command('node_groups')
-		if self.import_object:
+		if self.import_objects:
 			self.import_command('objects')
-		if self.import_paint_curve:
+		if self.import_paint_curves:
 			self.import_command('paint_curves')
-		if self.import_particle:
+		if self.import_particles:
 			self.import_command('particles')
-		if self.import_palette:
+		if self.import_palettes:
 			self.import_command('palettes')
-		if self.import_point_cloud:
+		if self.import_pointclouds:
 			self.import_command('pointclouds')
-		if self.import_light_probe:
+		if self.import_lightprobes:
 			self.import_command('lightprobes')
-		if self.import_scene:
+		if self.import_scenes:
 			self.import_command('scenes')
-		if self.import_sound:
+		if self.import_sounds:
 			self.import_command('sounds')
-		if self.import_speaker:
+		if self.import_speakers:
 			self.import_command('speakers')
-		if self.import_text:
+		if self.import_texts:
 			self.import_command('texts')
-		if self.import_texture:
+		if self.import_textures:
 			self.import_command('textures')
 		if self.import_volumes:
 			self.import_command('volumes')
-		if self.import_world:
+		if self.import_worlds:
 			self.import_command('worlds')
-		if self.import_workspace:
+		if self.import_workspaces:
 			self.import_command('workspaces')
 
 		return {'FINISHED'}
+
 
 classes = (IMPORT_SCENE_OT_tila_import_blend,)
 
---
+++ formats/import_module/unique_name/unique_name.py
@@ -0,0 +1,100 @@
+import bpy
+import re
+import uuid
+
+def unique_name_clean_func(name):
+	word_pattern = re.compile(r'(\.[0-9]{3})$', re.IGNORECASE)
+	name_iter = word_pattern.finditer(name)
+	name_iter_match = [w.group(1) for w in name_iter]
+
+	if len(name_iter_match) and name_iter_match[0] is not None:
+		return name.replace(name_iter_match[0], ''), True
+	else:
+		return name, False
+
+class UniqueName():
+	def __init__(self):
+		self.element_correspondance = {}
+	
+	@property
+	def uuid(self):
+		return uuid.uuid1().hex
+
+	def get_name_collisions_from_file(self, filepath, local_names):
+		name_collision = {}
+		# register name collision for objects in local_names
+		with bpy.data.libraries.load(filepath, link=False) as (data_from, _):
+			for name in data_from.objects:
+				if name in local_names:
+					name_collision[name] = f'{name}_{self.uuid}'
+			
+		return name_collision
+		
+	def get_next_valid_name(self, name):
+		return self.unique_name(-11, name, self.element_correspondance, clean_func=unique_name_clean_func, register=False)
+	
+	def get_unique_name(self, name):
+		return name + '_' + self.uuid
+	
+	def register_element_correspondance(self, elem):
+		if elem not in self.element_correspondance.keys():
+			self.element_correspondance[elem] = elem.name
+
+	# a modified version of bpy_extras.io_utils
+	def unique_name(self, key, name, name_dict, name_max=-1, clean_func=None, sep=".", register=True):
+		"""
+		Helper function for storing unique names which may have special characters
+		stripped and restricted to a maximum length.
+
+		:arg key: unique item this name belongs to, name_dict[key] will be reused
+		when available.
+		This can be the object, mesh, material, etc instance itself.
+		:type key: any hashable object associated with the *name*.
+		:arg name: The name used to create a unique value in *name_dict*.
+		:type name: string
+		:arg name_dict: This is used to cache namespace to ensure no collisions
+		occur, this should be an empty dict initially and only modified by this
+		function.
+		:type name_dict: dict
+		:arg clean_func: Function to call on *name* before creating a unique value.
+		:type clean_func: function
+		:arg sep: Separator to use when between the name and a number when a
+		duplicate name is found.
+		:type sep: string
+		"""
+		name_new = name_dict.get(key)
+		if name_new is None:
+			count = 1
+			has_number = False
+			name_dict_values = list(name_dict.values())
+
+			if clean_func is None:
+				name_new = name_new_orig = name
+			else:
+				name_new, has_number = clean_func(name)
+				name_new_orig = name_new
+			if has_number or name_new in name_dict_values:
+				if name_max == -1:
+					while name_new in name_dict_values:
+						name_new = "%s%s%03d" % (
+							name_new_orig,
+							sep,
+							count,
+						)
+						count += 1
+				else:
+					name_new = name_new[:name_max]
+					while name_new in name_dict_values:
+						count_str = "%03d" % count
+						name_new = "%.*s%s%s" % (
+							name_max - (len(count_str) + 1),
+							name_new_orig,
+							sep,
+							count_str,
+						)
+						count += 1
+
+			if register:
+				name_dict[key] = name_new
+
+		return name_new
