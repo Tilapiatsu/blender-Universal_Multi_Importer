fce0ac57dca7b7f24d5e15f99dc576e4b6542c2c
Tilapiatsu <tilapiatsu@hotmail.fr>
2024-06-22 00:48:42 +0200

    * move Command batcher presets to the proper folder

--- operators/ui/operators_const.py
+++ operators/ui/operators_const.py
@@ -2,5 +2,5 @@ import bpy
 import os
 
 PRESET_FOLDER = os.path.join(bpy.utils.script_paths()[1], 'presets')
-COMMAND_BATCHER_PRESET_FOLDER = os.path.join(PRESET_FOLDER, 'command_batcher')
+COMMAND_BATCHER_PRESET_FOLDER = os.path.join(PRESET_FOLDER, 'umi','command_batcher')
 UMIPRESET_EXTENSION = '.umipreset'
