ce436d1c1ce86b15926d8dcb1fb2a9a687beaa6a
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-07-05 23:06:57 +0200

    * update build script to generate two versions

--- scripts/zip_addon.py
+++ scripts/zip_addon.py
@@ -10,10 +10,12 @@ from typing import Union, Optional, Dict
 from pathlib import Path
 
 
-allowed_file_extensions = ('.py', 'LICENSE', '.md', '.dat', '.toml')
-ignore_folders = ('doc_assets', 'venv', 'tests', '.vscode', '__pycache__', 'site_package')
+allowed_file_extensions = (".py", "LICENSE", ".md", ".dat", ".toml")
+ignore_regular_folders = ("doc_assets", "venv", "tests", ".vscode", "__pycache__", "site_package")
+ignore_extension_folders = ignore_regular_folders + ("extensions",)
 
-def main(args: Optional[set]=None) -> None:
+
+def main(args: Optional[set] = None) -> None:
     """Command line entry point."""
     parser = argparse.ArgumentParser(
         description="Zip addon, and move it in a build folder",
@@ -29,20 +31,23 @@ def main(args: Optional[set]=None) -> None:
 
     addon_id: str = namespace.addon_id
 
-    zip_main(addon_id)
+    zip_main(addon_id, ignore_regular_folders, zip_suffix="_extension")
+    zip_main(addon_id, ignore_extension_folders)
 
-def get_addon_path(addon_name:str) -> Path:
+
+def get_addon_path(addon_name: str) -> Path:
     """Retreive the addon path."""
-    return Path(__file__).parent.parent / 'src' / addon_name
+    return Path(__file__).parent.parent / "src" / addon_name
 
-def zipdir(path:Path, ziph: zipfile.ZipFile, zip_subdir_name):
+
+def zipdir(path: Path, ziph: zipfile.ZipFile, zip_subdir_name, ignore_folders: tuple):
     """Zip the directory while exclude unnecessary files"""
     for root, dirs, files in os.walk(path):
         if any(root.__contains__(folder) for folder in ignore_folders):
             continue
 
         root = Path(root)
-        
+
         for file in files:
             if any(file.endswith(ext) for ext in allowed_file_extensions):
                 zip_subfolder_path = root.relative_to(path)
@@ -50,42 +55,49 @@ def zipdir(path:Path, ziph: zipfile.ZipFile, zip_subdir_name):
                 arc_hier = os.path.join(zip_subdir_name, zip_subfolder_path, file)
                 ziph.write(orig_hier, arc_hier)
 
-def generate_zip_filename(addon_name: str) -> str:
+
+def generate_zip_filename(addon_name: str, zip_suffix: str) -> str:
     """Generate an zip filename in this format : addon_id_vx.x.x"""
-    major, minor, patch = get_addon_version(get_addon_path(addon_name) / '__init__.py')
-    return '{}_v{}.{}.{}.zip'.format(addon_name, major, minor, patch)
+    major, minor, patch = get_addon_version(get_addon_path(addon_name) / "__init__.py")
+    return "{}{}_v{}.{}.{}.zip".format(addon_name, zip_suffix, major, minor, patch)
+
 
 def get_addon_version(init_path):
     """Retreive the addon version from bl_info in __init__.py."""
-    with open(init_path, 'r') as f:
+    with open(init_path, "r") as f:
         node = ast.parse(f.read())
 
     n: ast.Module
     for n in ast.walk(node):
         for b in n.body:
-            if isinstance(b, ast.Assign) and isinstance(b.value, ast.Dict) and (
-                    any(t.id == 'bl_info' for t in b.targets)):
+            if (
+                isinstance(b, ast.Assign)
+                and isinstance(b.value, ast.Dict)
+                and (any(t.id == "bl_info" for t in b.targets))
+            ):
                 bl_info_dict = ast.literal_eval(b.value)
-                return bl_info_dict['version']
-    raise ValueError('Cannot find bl_info')
+                return bl_info_dict["version"]
+    raise ValueError("Cannot find bl_info")
 
-def zip_main(addon_name: str):
-    """ Zip the addon."""
-    filename = generate_zip_filename(addon_name)
+
+def zip_main(addon_name: str, ignore_folders: tuple, zip_suffix: str = ""):
+    """Zip the addon."""
+    filename = generate_zip_filename(addon_name, zip_suffix)
     lower_name = addon_name.lower()
-    destination = Path(__file__).parent.parent / 'builds'
+    destination = Path(__file__).parent.parent / "builds"
     if not os.path.exists(destination):
         os.mkdir(destination)
     filepath = os.path.join(destination, filename)
     try:
-        zipf = zipfile.ZipFile(filepath, 'w', zipfile.ZIP_DEFLATED)
-        zipdir(get_addon_path(addon_name), zipf, lower_name)
+        zipf = zipfile.ZipFile(filepath, "w", zipfile.ZIP_DEFLATED)
+        zipdir(get_addon_path(addon_name), zipf, lower_name, ignore_folders=ignore_folders)
         zipf.close()
-        print('Successfully created zip file: {}'.format(filepath))
+        print("Successfully created zip file: {}".format(filepath))
         return filepath
     except Exception as e:
-        print('Failed to create {}: {}'.format(filename, e))
+        print("Failed to create {}: {}".format(filename, e))
         return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
